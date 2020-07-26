d9c83cec99f7a3081d11fb20e1caf5bbdab1c77a
LEMON\Moriko <moriko232@gmail.com>
2020-07-26 19:06:30 +0800

    update week5

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 ## Demo link
 
 [Week 4  - Vue 與 API 串接](https://moriko232.github.io/vue-practice/week4/index.html)
+
+[Week 5  - Vue 與 API 串接](https://moriko232.github.io/vue-practice/week5/index.html)
+
---
+++ week5/script.js
@@ -0,0 +1,171 @@
+import zh from './zh_TW.js';
+
+// VeeValidate語系包
+VeeValidate.localize('tw', zh);
+
+// VeeValidate custom class
+VeeValidate.configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
+  }
+});
+
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+
+Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
+
+const uuid = "31e50ae2-761f-42e4-b9da-0c081fdb851d";
+const getProductUrl = `https://course-ec-api.hexschool.io/api/${uuid}/ec/products`;
+const editShopCartUrl = `https://course-ec-api.hexschool.io/api/${uuid}/ec/shopping`;
+const editShopCartAllUrl = `https://course-ec-api.hexschool.io/api/${uuid}/ec/shopping/all/product`;
+const sendOrderUrl = `https://course-ec-api.hexschool.io/api/${uuid}/ec/orders`;
+
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    isLoading: false,
+    products: [],
+    shopCart: [],
+    customer: {
+      name: '',
+      email: '',
+      tel: '',
+      address: '',
+      payment: 'WebATM',
+      message: ''
+    },
+    messageModelOpen: false,
+    messageText: '',
+  },
+  mounted() {
+    this.getDataApi()
+    this.getShopCartApi()
+  },
+  computed: {
+    total() {
+      let sum = 0
+      this.shopCart.forEach((item) => {
+        sum += (item.product.price * item.quantity)
+      })
+      return sum
+    }
+  },
+  methods: {
+    openMessage(msg) {
+      this.messageText = msg
+      $("#messageModal").modal("show");
+      this.messageModelOpen = true
+    },
+    closeMessage() {
+      this.messageText = ''
+      $("#messageModal").modal("hide");
+      this.messageModelOpen = false
+    },
+    sendOrderApi() {
+      axios
+        .post(sendOrderUrl, this.customer)
+        .then((res) => {
+            this.openMessage('已收到您的訂單!')
+            this.getShopCartApi()
+          this.shopCart = [];
+        })
+        .catch(function (err) {
+          console.log("err", err);
+        });
+    },
+    getShopCartApi() {
+      axios
+        .get(editShopCartUrl)
+        .then((res) => {
+          this.shopCart = res.data.data;
+        })
+        .catch(function (err) {
+          console.log("err", err);
+        });
+    },
+    isAlreadyInCart(id) {
+      return this.shopCart.findIndex((item) => {
+        return item.id === id
+      })
+    },
+    deleteCartAll() {
+      this.isLoading = true
+      axios
+        .delete(editShopCartAllUrl)
+        .then((res) => {
+          this.shopCart = res.data.data;
+          this.getDataApi()
+          this.getShopCartApi()
+          this.isLoading = false;
+        })
+    },
+    deleteCartItem(id) {
+      this.isLoading = true
+      const data = {
+        product: id
+      }
+      axios
+        .delete(`${editShopCartUrl}/${id}`)
+        .then((res) => {
+          this.shopCart = res.data.data;
+          this.getDataApi()
+          this.getShopCartApi()
+          this.isLoading = false;
+        })
+    },
+    editCart(id, quantity = 1) {
+      this.isLoading = true
+      const data = {
+        product: id,
+        quantity: quantity
+      }
+      axios
+        .patch(editShopCartUrl, data)
+        .then((res) => {
+          this.shopCart = res.data.data;
+
+          this.getDataApi()
+          this.getShopCartApi()
+          this.isLoading = false;
+        })
+        .catch(function (err) {
+          console.log("err", err);
+        });
+    },
+    addToCart(item, quantity = 1) {
+      // const order = this.isAlreadyInCart(item.id)
+
+      this.isLoading = true
+      const data = {
+        product: item.id,
+        quantity: quantity
+      }
+      axios
+        .post(editShopCartUrl, data)
+        .then((res) => {
+          this.shopCart = res.data.data;
+
+          this.getDataApi()
+          this.getShopCartApi()
+          this.isLoading = false;
+        })
+        .catch(function (err) {
+          console.log("err", err);
+        });
+
+
+    },
+    getDataApi() {
+      axios
+        .get(getProductUrl)
+        .then((res) => {
+          this.products = res.data.data;
+        })
+        .catch(function (err) {
+          console.log("err", err);
+        });
+    },
+  },
+})
